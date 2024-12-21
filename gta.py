e79234fb3d97d90a70b8cc1043fae7e3202d41d1
idk-aarza <aarzak2012@gmail.com>
2024-12-21 10:46:40 +0530

    hh

---
+++ gta.py
@@ -0,0 +1,11 @@
+tupple=(9,3,"a","b")
+print(tupple)
+tupplea=(5,10,"e","f")
+print(tupplea)
+a=tupple+tupplea
+print(a)
+print(len(tupple))
+for item in tupple:
+    print(item)
+print(tupple[2:4])
+print(tupplea.count(5))
